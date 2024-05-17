69eae5b32e1bf0a3d3e6f546918dab768eba1481
Jondrewd <jonas220703@gmail.com>
2024-05-17 10:52:46 -0300

    Update UserService.java

--- src/main/java/com/jondrewd/workshopmongo/services/UserService.java
+++ src/main/java/com/jondrewd/workshopmongo/services/UserService.java
@@ -1,8 +1,10 @@
 package com.jondrewd.workshopmongo.services;
 import com.jondrewd.workshopmongo.domain.User;
 import com.jondrewd.workshopmongo.repository.UserRepository;
+import com.jondrewd.workshopmongo.services.exception.ObjectNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,9 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();    
+    }
+    public User findById(String id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
     }
 }
