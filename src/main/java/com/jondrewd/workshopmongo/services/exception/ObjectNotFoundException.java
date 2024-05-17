2ec29635c7116b45aa324fe036696060d4cfb12f
Jondrewd <jonas220703@gmail.com>
2024-05-17 10:52:36 -0300

    tratamento de exceções

---
+++ src/main/java/com/jondrewd/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jondrewd.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID =1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
